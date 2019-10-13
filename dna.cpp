597eada594afb573b48de68790a49f057d581414
Amogh13246831 <42728157+Amogh13246831@users.noreply.github.com>
2019-10-13 21:32:11 +0530

    Add files via upload

--- dna.cpp
+++ dna.cpp
@@ -65,26 +65,28 @@ string int_to_binary(int c, int num_bits) {
   }
   return bin;
 }
-
-int main(int argc, char **argv) {
-  cout<<"DNA Based Encryption\n\n";
-  
-  string bases[] = {"A", "T", "G", "C"};
+/*
+class EncryptorDecryptor {
+  private:
   CryptoKey k;
+  static string bases[4];
+  char buffer[1];
+  int codon_size;
+  string dna;
+  string plaintext;
+  public:
+  string encrypt_file(string in_file, string out_file);
+  string decrypt_file(string in_file, string out_file);
+};
+*/
+// encrypt a file with DNA encryptor
+string encrypt_file(string in_file, string out_file, CryptoKey k) {
+  string bases[] = {"A", "T", "G", "C"};
   string dna = "", plaintext = "";
   int codon_size = 2;
   int max_bits_per_num = 2 * codon_size;
 
-  cout<<"Cryptographic Key:\n";
-  k.keygen(2, 1);
-  for(auto itr=k.key.begin(); itr!=k.key.end(); itr++)
-    cout<<itr->first<<"\t"<<itr->second<<endl;
-  cout<<endl;
-  
-  if(argc < 2)
-    return 0;
-
-  ifstream in(argv[1], ios::in | ios::binary);
+  ifstream in(in_file, ios::in | ios::binary);
   char buffer[1];
   while(in.read(buffer, 1)) {
     plaintext += buffer[0];
@@ -95,6 +97,7 @@ int main(int argc, char **argv) {
     dna += bases[buffer[0] & 0x03];
     //cout<<buffer[0]<<"\t"<<dna<<endl;
   }
+  in.close();
   cout<<"Plaintext:\n"<<plaintext<<endl;
   cout<<"DNA String\n"<<dna<<endl<<endl;
   
@@ -105,8 +108,91 @@ int main(int argc, char **argv) {
     converted = k.key.find(codon)->second;
     cout<<codon<<"\t"<<converted<<endl;
     binary_cipher += int_to_binary(converted, max_bits_per_num);
+    // out.write(4 bits of converted) to store actual bits
   }
   cout<<endl;
-  cout<<"Binary Ciphertext:\n"<<binary_cipher<<endl;
+  cout<<"Binary Ciphertext:\n"<<binary_cipher<<endl<<endl;
+
+  ofstream out(out_file, ios::out | ios::binary);
+  int write_num;
+  for(int i=0; i<dna.length(); i+=4) {
+    // first 4 bits
+    codon = dna.substr(i, 2);
+    converted = k.key.find(codon)->second;
+    write_num = (converted << 4) & 0xf0;
+    // next 4 bits
+    codon = dna.substr(i+2, 2);
+    converted = k.key.find(codon)->second;
+    write_num += converted & 0x0f;
+    // write char
+    buffer[0] = (char) write_num;  // only lower 8 bits
+    out.write(buffer, 1);
+  }
+  out.close();
+  
+  return binary_cipher;
+}
+
+int match_letter(char base) {
+  return base == 'A'? 0: base == 'T'? 1: base =='G'? 2: 3;
+}
+
+// decrypt a file encrypted with DNA encryptor
+string decrypt_file(string in_file, string out_file, CryptoKey k) {
+  int codon_size = 2;
+  int max_bits_per_num = 2 * codon_size;
+  
+  map<int, string> reverse_key;   // reverse lookup
+  for(auto itr=k.key.begin(); itr!=k.key.end(); itr++)
+    reverse_key.insert(pair<int, string>(itr->second, itr->first));
+
+  cout<<"Reversed key for decryption:\n";
+  for(auto itr=reverse_key.begin(); itr!=reverse_key.end(); itr++)
+    cout<<itr->first<<"\t"<<itr->second<<endl;
+  cout<<endl;
+
+  ifstream in(in_file, ios::in | ios::binary);
+  char buffer[1];
+  int received;
+  string dna;
+  while(in.read(buffer, 1)) {
+    // append correct codon to dna
+    received = (int) (buffer[0]>>4 & 0x0f);
+    dna += reverse_key.find(received)->second;
+    received = (int) (buffer[0] & 0x0f);
+    dna += reverse_key.find(received)->second;
+  }
+  cout<<"DNA String\n"<<dna<<endl;
+
+  string bases[] = {"A", "T", "G", "C"};
+  char letter = 0;
+  string plaintext = "";
+  for(int i=0; i<dna.length(); i+=4) {
+    letter = (match_letter(dna[i]) << 6) & 0xc0;
+    letter += (match_letter(dna[i+1]) << 4) & 0x30;
+    letter += (match_letter(dna[i+2]) << 2) & 0x0c;
+    letter += match_letter(dna[i+3]) & 0x03;
+    plaintext += letter;
+  }
+  cout<<"Plaintext:\n"<<plaintext<<endl;
+
+  return plaintext;
+}
+
+int main(int argc, char **argv) {
+  cout<<"DNA Based Encryption\n\n";
+
+  CryptoKey k;
+  cout<<"Cryptographic Key:\n";
+  k.keygen(2, 1);
+  for(auto itr=k.key.begin(); itr!=k.key.end(); itr++)
+    cout<<itr->first<<"\t"<<itr->second<<endl;
+  cout<<endl;
+  
+  if(argc < 2)
+    return 0;
+ 
+  encrypt_file(argv[1], "test.crypt", k);
+  decrypt_file("test.crypt", "", k);
   return 0;
 }
